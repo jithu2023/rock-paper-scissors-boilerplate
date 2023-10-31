457cbbe78bc6cf12c50014dd20327976e7876128
jithu2023 <jithumon.j@kalvium.community>
2023-10-31 10:35:50 +0530

    game

---
+++ front.js
@@ -0,0 +1,7 @@
+const clickableImage = document.getElementById("clickable-image");
+        
+
+clickableImage.addEventListener("click", function() {
+   
+    window.location.href = "index.html"; 
+});
